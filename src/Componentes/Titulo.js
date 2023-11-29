b34813458f7577e1a20b337c8cdc6fc778350848
federicoamigo17 <federicoamigo17@gmail.com>
2023-11-29 12:14:17 -0300

    estilos.css y titulo

--- src/Componentes/Titulo.js
+++ src/Componentes/Titulo.js
@@ -1,9 +1,10 @@
 import React from "react";
+import "../css/Estilos.css";
 
 function Titulo() {
   return (
     <div>
-    <h1>Planilla Informe Avance(Grande Rojo)</h1>
+    <h1 className="titulo-pagina">Planilla Informe Avance(Grande Rojo)</h1>
     <h2>DOCTORADO EN INGENIERIA(Titulo en negrita)</h2>
     <h3>Informe anual del director(Titulo en negrita + chico)</h3>
     </div>
